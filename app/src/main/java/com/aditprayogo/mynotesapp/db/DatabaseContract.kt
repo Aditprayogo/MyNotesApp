7513fd918467531e62592af0d1b83c929f93b323
Aditprayogo <adit.ihzar@gmail.com>
2020-03-28 21:12:16 +0700

    membuat URI content

--- app/src/main/java/com/aditprayogo/mynotesapp/db/DatabaseContract.kt
+++ app/src/main/java/com/aditprayogo/mynotesapp/db/DatabaseContract.kt
@@ -1,15 +1,29 @@
 package com.aditprayogo.mynotesapp.db
 
+import android.net.Uri
 import android.provider.BaseColumns
 
-internal class DatabaseContract {
+object DatabaseContract {
+
+    const val AUTHORY = "com.aditprayogo.mynotesapp"
+    const val SCHEME = "content"
+
     internal class NoteColumns : BaseColumns {
+
         companion object {
             const val TABLE_NAME = "note"
             const val _ID = "_id"
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
+
+            /**
+             * Untuk membuat URI content
+             */
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORY)
+                .appendPath(TABLE_NAME)
+                .build()
         }
     }
 }
